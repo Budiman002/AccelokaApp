5ff829a9108227dca9f71e4c034d7e9bdb642c75
Arif Budiman <arifb5624@gmail.com>
2026-02-25 18:43:36 +0700

    init Next.js frontend with Tailwind, shadcn/ui for compenent and API Service

---
+++ frontend/lib/services/api.ts
@@ -0,0 +1,21 @@
+const API_BASE_URL = "https://localhost:7039/api/v1";
+
+export async function fetchApi<T>(
+  endpoint: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options?.headers,
+    },
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
